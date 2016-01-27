931d91f28da4264633d2b27cbbaa26d2c80a2c60
brad clawsie <brad@b7j0c.org>
2016-01-27 14:09:26 -0800

    add tests

--- httpdshutdown_test.go
+++ httpdshutdown_test.go
@@ -2,8 +2,13 @@ package httpdshutdown
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net"
 	"net/http"
+	"net/http/httptest"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestNil(t *testing.T) {
@@ -82,4 +87,132 @@ func TestStop(t *testing.T) {
 	if err != nil {
 		t.Errorf("TestStop: should not have an error")
 	}
+}
+
+func TestHttpDaemonTimeout(t *testing.T) {
+	fmt.Printf("\n\n")
+	w,w_err := NewWatcher(3000,sampleShutdownHook)
+	if w == nil || w_err != nil {
+		t.Errorf("TestHttpDaemonTimeout: should not be nil")
+	}
+	err := w.Accepting(true)
+	if err != nil {
+		t.Errorf("TestHttpDaemonTimeout: should not have error")
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello from the test daemon; sleeping")
+		time.Sleep(5 * time.Second)
+		fmt.Println("goodbye from the test daemon")
+		fmt.Fprintln(w, "Hello, client")
+		return
+	}))
+
+	ts.Config.ConnState = func(conn net.Conn, newState http.ConnState) {
+		fmt.Printf("(0) NEW CONN STATE:%v\n",newState)
+		w.RecordConnState(newState)
+		return
+	}
+	// needed to force the connection to close
+	ts.Config.ReadTimeout = 15 * time.Second
+	ts.Config.WriteTimeout = 15 * time.Second
+
+	ts.Start()
+	defer ts.Close()
+
+	var wg sync.WaitGroup
+
+	fmt.Println("watcher should trigger before handler completes: should see a timeout message")
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("about to call handler")
+		getResp, getErr := http.Get(ts.URL)
+		if getErr != nil {
+			t.Errorf(getErr.Error())
+		}
+		_, readErr := ioutil.ReadAll(getResp.Body)
+		getResp.Body.Close()
+		if readErr != nil {
+			t.Errorf(readErr.Error())
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("about to call OnStop, sleep 1 first")
+		time.Sleep(1 * time.Second)
+		err := w.OnStop()
+		if err == nil {
+			t.Errorf("TestHttpDaemonTimeout: should have an error, a timeout was supposed to occur")
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func TestHttpDaemonNormalExit(t *testing.T) {
+	fmt.Printf("\n\n")
+	w,w_err := NewWatcher(20000,sampleShutdownHook)
+	if w == nil || w_err != nil {
+		t.Errorf("TestHttpDaemonNormalExit: should not be nil")
+	}
+	err := w.Accepting(true)
+	if err != nil {
+		t.Errorf("TestHttpDaemonNormalExit: should not have error")
+	}
+
+	ts := httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("hello from the test daemon; sleeping")
+		time.Sleep(1 * time.Second)
+		fmt.Println("goodbye from the test daemon")
+		fmt.Fprintln(w, "Hello, client")
+		return
+	}))
+
+	ts.Config.ConnState = func(conn net.Conn, newState http.ConnState) {
+		fmt.Printf("(1) NEW CONN STATE:%v\n",newState)
+		w.RecordConnState(newState)
+		return
+	}
+	// needed to force the connection to close
+	ts.Config.ReadTimeout = 3 * time.Second
+	ts.Config.WriteTimeout = 3 * time.Second
+
+	ts.Start()
+	defer ts.Close()
+
+	var wg sync.WaitGroup
+
+	fmt.Println("watcher should not trigger before handler completes: should see no timeout message")
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("about to call handler")
+		getResp, getErr := http.Get(ts.URL)
+		if getErr != nil {
+			t.Errorf(getErr.Error())
+		}
+		_, readErr := ioutil.ReadAll(getResp.Body)
+		getResp.Body.Close()
+		if readErr != nil {
+			t.Errorf(readErr.Error())
+		}
+		wg.Done()
+	}()
+
+	wg.Add(1)
+	go func() {
+		fmt.Println("about to call OnStop, sleep 1 first")
+		time.Sleep(1 * time.Second)
+		err := w.OnStop()
+		if err != nil {
+			t.Errorf("TestHttpDaemonNormalExit: should have no error, no timeout was supposed to occur")
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
 }
